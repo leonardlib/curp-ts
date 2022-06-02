78180ef4ba2f6db3f7f9e453aab9f3995e512b6c
Leonardo Lira Becerra <leonardolirabecerra@gmail.com>
2022-06-01 21:12:45 -0500

    [FEATURE] initial template

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
+};
